c1008cece897d25cc094921753cb9c6df5e4421f
Fitto <fitto@cmu.ac.th>
2022-12-09 15:53:49 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int number;
+    int even=0;
+    int odd=0;
+
+    do
+    {
+        cout << "Enter an integer: ";
+        cin >> number;
+        if(number%2 == 0 && number != 0)
+        {
+            even++;
+        }
+        if(number%2 != 0){
+            odd++;
+        }
+    } while(number != 0);
+    
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
